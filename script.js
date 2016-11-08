097025169ddcffa6cbee815958b90f70cefd87d0
vishal <vishal.chaturvedi@systematix.com>
2016-11-08 14:38:59 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+angular.module('app', [])
+  .directive('scrollTo', function ($location, $anchorScroll) {
+    return function(scope, element, attrs) {
+    element.bind('click', function(event) {
+			event.stopPropagation();
+			scope.$on('$locationChangeStart', function(ev) {
+			  ev.preventDefault();
+			});
+			var location = attrs.scrollTo;
+			$location.hash(location);
+			$anchorScroll();
+		});
+	};
+  });
